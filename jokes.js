953cbcb8d49bd57e5304f5531a427a8e8fa41b3e
Lalit Jamdagnee <110276457+lalit-jamdagnee@users.noreply.github.com>
2024-07-29 00:28:27 +0530

    Update jokes.js
    Added hover affects on the refresh page button

--- jokes.js
+++ jokes.js
@@ -105,3 +105,23 @@ a.textContent = 'Click to refresh';
 a.setAttribute('id','refresh');
 
 document.getElementsByClassName('jokes')[0].appendChild(a);
+
+//To add hover affect hover affect on the refresh button
+let btn = document.getElementById('refresh');
+
+function over(){
+    btn.style.color = 'green';
+    btn.style.opacity = 1;
+    btn.style.fontSize = "15px";
+}
+function left(){
+    btn.style.color = 'white';
+    btn.style.opacity = 0.5;
+    btn.style.fontSize = "14px";
+}
+let x = function(e){
+    location.reload(true);
+}
+btn.addEventListener('mouseover', over);
+btn.addEventListener('mouseout', left)
+btn.addEventListener('click', x)

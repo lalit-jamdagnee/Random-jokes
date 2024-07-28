f7f41399f0a45469b38be8cb3855aa29e17fb1c9
Lalit Jamdagnee <110276457+lalit-jamdagnee@users.noreply.github.com>
2024-07-29 00:32:57 +0530

    Delete script.js
    Extended it's code to the jokes.js file

--- script.js
+++
@@ -1,8 +0,0 @@
-let navBar = document.getElementsByClassName('navigation')[0]
-navBar.style.background = 'black';
-navBar.firstElementChild.style.color = 'orangered';
-navBar.lastElementChild.style.color = 'green';
-
-Array.from(document.getElementsByTagName('h3')).forEach((element) =>{
-    element.style.background = 'black';
-})
